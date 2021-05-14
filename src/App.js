e610a743c3dfed9690fdd7b2935b3518c465ee5f
= <forgithubs@protonmail.com>
2021-05-13 23:01:12 -0400

    Fix scroll

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ const useStyles = makeStyles({
   scrollable: {
     overflowY: 'auto',
     overflowX: 'hidden',
-    maxHeight: 800
   },
   dark: {
     background: "rgb(17,0,36) linear-gradient(240deg, rgba(17,0,36,1) 3%, rgba(32,56,171,1) 43%, rgba(27,5,71,0.8827906162464986) 100%)",
@@ -108,7 +107,7 @@ function App() {
      <CssBaseline>
           <Grid container className={classes.root}>
             <Grid item xs={3}></Grid>
-            <Grid item xs={7} className={classes.scrollable}>
+            <Grid item xs={7} className={classes.scrollable} style={{maxHeight:windowDimensions.height}}>
               <GridList>
                 <Feed />
               </GridList>
@@ -121,129 +120,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-  // async function fetchGreeting() {
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum)
-  //     console.log({ provider })
-  //     const contract = new ethers.Contract(mynftAddress, Greeter.abi, provider)
-  //     try {
-  //       const data = await contract.greet()
-  //       console.log('data: ', data)
-  //     } catch (err) {
-  //       console.log("Error: ", err)
-  //     }
-  //   }
-  // }
-  //
-  // async function getBalance() {
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
-  //     const balance = await contract.balanceOf(account);
-  //     console.log("Balance: ", balance.toString());
-  //   }
-  // }
-  //
-  // async function setGreeting() {
-  //   if (!greeting) return
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     await requestAccount()
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     console.log({ provider })
-  //     const signer = provider.getSigner()
-  //     const contract = new ethers.Contract(mynftAddress, Greeter.abi, signer)
-  //     const transaction = await contract.setGreeting(greeting)
-  //     await transaction.wait()
-  //     fetchGreeting()
-  //   }
-  // }
-  //
-  // async function sendCoins() {
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     await requestAccount()
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     const signer = provider.getSigner();
-  //     const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
-  //     const transation = await contract.transfer(userAccount, amount);
-  //     await transation.wait();
-  //     console.log(`${amount} Coins successfully sent to ${userAccount}`);
-  //   }
-  // }
-
-  // async function connect() {
-  //   if (typeof window.ethereum !== 'undefined') {
-  //     await requestAccount()
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     const signer = provider.getSigner();
-  //     const contract = new ethers.Contract(myNftAddress, MyNft.abi, signer);
-  //     const transation = await contract.awardItem(nftTargetAccount, "mytoken.uri");
-  //     await transation.wait();
-  //     console.log(`${amount} Coins successfully sent to ${userAccount}`);
-  //   }
-  // }
-
-
-
-// import './App.css';
-// import { useState } from 'react';
-// import { ethers } from 'ethers'
-// import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
-//
-// // Update with the contract address logged out to the CLI when it was deployed
-// //const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-// const greeterAddress = "0xD5b480eAC3e91A1167443662D2f12Ca64F4cC7D5"
-// function App() {
-//   // store greeting in local state
-//   const [greeting, setGreetingValue] = useState()
-//
-//   // request access to the user's MetaMask account
-//   async function requestAccount() {
-//     await window.ethereum.request({ method: 'eth_requestAccounts' });
-//   }
-//
-//   // call the smart contract, read the current greeting value
-//   async function fetchGreeting() {
-//     if (typeof window.ethereum !== 'undefined') {
-//       const provider = new ethers.providers.Web3Provider(window.ethereum)
-//       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-//       try {
-//         const data = await contract.greet()
-//         console.log('data: ', data)
-//       } catch (err) {
-//         console.log("Error: ", err)
-//       }
-//     }
-//   }
-//
-//   // call the smart contract, send an update
-//   async function setGreeting() {
-//     if (!greeting) return
-//     if (typeof window.ethereum !== 'undefined') {
-//       await requestAccount()
-//       const provider = new ethers.providers.Web3Provider(window.ethereum);
-//       const signer = provider.getSigner()
-//       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-//       const transaction = await contract.setGreeting(greeting)
-//       await transaction.wait()
-//       fetchGreeting()
-//     }
-//   }
-//
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <button onClick={fetchGreeting}>Fetch Greeting</button>
-//         <button onClick={setGreeting}>Set Greeting</button>
-//         <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
